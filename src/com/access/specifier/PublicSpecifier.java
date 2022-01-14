aefb264067152ed0420492fc17e3b671377698fa
naresh9492 <nareshkuruba1998@gmail.com>
2022-01-14 22:09:38 +0530

    Added Public Specifier

---
+++ src/com/access/specifier/PublicSpecifier.java
@@ -0,0 +1,11 @@
+package com.access.specifier;
+
+public class PublicSpecifier {
+
+	public void msg() {
+		System.out.println("Heello");
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/access/specifier/Pubspecifier.java
@@ -0,0 +1,9 @@
+package com.access.specifier;
+import java.util.*;
+public class Pubspecifier {
+	public static void main(String[] args) {
+		PublicSpecifier obj = new PublicSpecifier();
+		obj.msg();
+	}
+
+}
