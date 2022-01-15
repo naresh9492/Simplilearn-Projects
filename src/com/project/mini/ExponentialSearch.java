7f3b8bf7c032547081b8459683006fb0375daa75
naresh9492 <nareshkuruba1998@gmail.com>
2022-01-15 15:39:54 +0530

    Added Exponential Search

---
+++ src/com/project/mini/ExponentialSearch.java
@@ -0,0 +1,54 @@
+package com.project.mini;
+import java.util.*;
+public class ExponentialSearch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//Scanner class to take input from user
+		Scanner sc = new Scanner(System.in);
+		int[] arr = {4,2,34,84,56,35,5,6};
+		//sort the array elements using sort()method
+		Arrays.sort(arr);
+		System.out.println("Sorted Array-" + Arrays.toString(arr) );
+		System.out.println("enter value to search:");
+		int ele = sc.nextInt();
+		int i = exponentialSearch(arr, ele);
+		if(i!= -1){
+		      System.out.println("Searched item " + arr[i] + " found at index "+i);
+		    }else{
+		      System.out.println("Searched item " + ele + " not found in the array");
+		    }
+		    sc.close();
+		  }
+		    
+		  private static int exponentialSearch(int[] arr, int searchElement){
+		    int bound = 1;
+		    // increase upper bound 
+		    while (bound < arr.length && arr[bound] < searchElement) {
+		      bound *= 2;
+		    }
+		    // do binary search with in the range
+		    return binarySearch(arr, bound/2, Integer.min(bound + 1, arr.length), searchElement);
+		  }
+
+		  private static int binarySearch(int[] arr, int start, int end, int searchElement){
+		    // exit condition
+		    if(start > end){
+		      return -1;
+		    }        
+		    int middle = (start+end)/2;
+		    // element found
+		    if(searchElement == arr[middle]){
+		      return middle;
+		    }
+		    // left half
+		    if(searchElement < arr[middle]){
+		      return binarySearch(arr, start, middle-1, searchElement);
+		    }else{
+		      // right half
+		      return binarySearch(arr, middle+1, end, searchElement); 
+		
+	}
+
+		  }
+}
