9494039e8acbd5a6b52ea633e2a8ae27ed4ad9c4
naresh9492 <nareshkuruba1998@gmail.com>
2022-01-15 23:14:28 +0530

    Added Custom Exceptions

---
+++ src/com/custom/exception/FinallyBlock.java
@@ -0,0 +1,22 @@
+package com.custom.exception;
+
+public class FinallyBlock {
+    public static void main(String[] args)
+    {
+        int a=45,b=0,rs=0;
+        try
+        {
+            rs = a / b;
+        }
+        catch(ArithmeticException Ex)
+        {
+            System.out.print("\n\tError : " + Ex.getMessage());
+        }
+        finally
+        {
+            System.out.print("\n\tThe result is : " + rs);
+        }
+    }
+
+
+}
