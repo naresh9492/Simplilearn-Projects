4d8697628567ec6ed3678d33e48341b63dcf8119
naresh9492 <nareshkuruba1998@gmail.com>
2022-01-15 15:20:00 +0530

    Added linearSearch

---
+++ src/com/project/mini/linearSearch.java
@@ -0,0 +1,34 @@
+package com.project.mini;
+import java.util.Scanner;
+public class linearSearch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i, key, array[];
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter Array Length : ");
+		int len = input.nextInt();
+		array = new int[len];
+		System.out.println("Enter " + len + " elements");
+		 
+	      for (i = 0; i < len; i++)
+	      {
+	        array[i] = input.nextInt();
+	      }
+	      System.out.println("Enter the search key value:");
+	      key = input.nextInt();
+	      for (i = 0; i < len; i++)
+	      {
+	         if (array[i]== key) 
+	         {
+	           System.out.println(key+" is present at location "+(i+1));
+	            break;
+	         }
+	      }
+	      if (i == len)
+	        System.out.println(key + " doesn't exist in array.");
+		
+
+	}
+
+}
