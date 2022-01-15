549dda549815f96774dfdad2e355d0a51394cf0d
naresh9492 <nareshkuruba1998@gmail.com>
2022-01-15 13:12:35 +0530

    Added ArrayAssisted

---
+++ src/com/project/mini/ArrayAssisted.java
@@ -0,0 +1,25 @@
+package com.project.mini;
+
+public class ArrayAssisted {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//single dimensional array
+		int[] arr = {34,35,46,54,76};
+		for (int i = 0;i<arr.length;i++) {
+			System.out.println(arr[i]);
+		}
+//multidimensional array
+		int[][] b = {
+	            {2, 4, 6, 8}, 
+	            {3, 6, 9} };
+	      
+	      System.out.println("\nLength of row 1: " + b[0].length);
+	      }
+		
+			
+		
+		
+	}
+
+
